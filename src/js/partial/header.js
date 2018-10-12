9e3b2869cdf7b93845b43034c0903528615b0256
Zoommie <zdinhsd@gmail.com>
2018-10-11 19:22:08 -0700

    style admin dashboard

---
+++ src/js/partial/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class header extends Component {
+  render() {
+    return (
+      <div>
+         <img src='images/logo.png' className='d-inline-block' />
+      </div>
+    )
+  }
+}
+
+export default header;
