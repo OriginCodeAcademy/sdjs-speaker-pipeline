eef30bd7e94673c61fddf074ca911f62e3b1bdc8
Zamarise Montoya <zamarise@gmail.com>
2018-10-19 17:01:57 -0700

    Add landing component to route path '/'

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import AdminDashboardWithRouter from './components/AdminDashboard/index';
 import AdminLogin from './components/AdminLogin/index';
 import EditSpeakerData from './components/EditSpeakerData/EditSpeakerData';
-
 
 class App extends Component {
   render() {
     return (
       <div>
-
         <Router>
           <div>
-            <Route exact path="/" />
+            <Route exact path="/" component={Landing} />
             <Route path="/login" component={AdminLogin} />
             <Route path="/admin" component={AdminDashboardWithRouter} />
             <Route path="/edit" component={EditSpeakerData} />
