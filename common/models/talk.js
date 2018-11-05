8767af02811ce54c9eb1713c8164ff0d9a49043c
Austin <austin.gray4292@gmail.com>
2018-11-05 10:15:26 -0800

    refines data received from meetup api

--- common/models/talk.js
+++ common/models/talk.js
@@ -2,23 +2,23 @@
 
 const { getMeetups } = require('../../server/utils/getMeetups');
 
-module.exports = function(Talk) { 
-    Talk.getMeetups = function(cb) {
-        getMeetups()
-            .then(meetups => cb(null, meetups))
-            .catch(err => cb(err))
-    }
+module.exports = function (Talk) {
+	Talk.getMeetups = function (cb) {
+		getMeetups()
+			.then(meetups => cb(null, meetups))
+			.catch(err => cb(err))
+	}
 
-    Talk.remoteMethod('getMeetups', {
-        description: 'Gets all SDJS meetups for the next 3 months.',
-        http: {
-            path: '/getMeetups',
-            verb: 'get'
-        },
-        returns: {
-            arg: 'data',
-            type: 'array',
-            root: true
-        }
-    })
+	Talk.remoteMethod('getMeetups', {
+		description: 'Gets all SDJS meetups for the next 3 months.',
+		http: {
+			path: '/getMeetups',
+			verb: 'get'
+		},
+		returns: {
+			arg: 'data',
+			type: 'array',
+			root: true
+		}
+	})
 };
