97dfe7dcc69ec6a9018c147a9ef3bf5a2e9d7d23
BrianC <seahorse8789@gmail.com>
2018-11-13 17:38:39 -0800

    made more changes after rebase because rebase removed somes styles.

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -2,19 +2,28 @@ import React, { Component } from 'react';
 import { Field } from 'react-redux-form';
 import { connect } from 'react-redux';
 import {
-	onChange,
 	talkSubmit,
 	getDates,
+	updateSpeakerName,
+	updateEmail,
+	updatePhone,
+	updateDate,
+	updateTopic,
+	updateDescription,
 } from './SignUpActions'
 import Navbar from '../Navbar/Navbar';
-import moment from 'moment';
-
 
 class SignUp extends Component {
 	constructor(props) {
 		super(props);
 
-		this.handleChange 	= this.handleChange.bind(this);
+		this.handleSpeakerName 	= this.handleSpeakerName.bind(this);
+		this.handleEmail 		= this.handleEmail.bind(this);
+		this.handlePhone 		= this.handlePhone.bind(this);
+		this.handleDate 		= this.handleDate.bind(this);
+		this.handleTopic 		= this.handleTopic.bind(this);
+		this.handleDescription 	= this.handleDescription.bind(this);
+		this.submitSpeaker 		= this.submitSpeaker.bind(this);
 	}
 
 	componentDidMount() {
@@ -22,11 +31,30 @@ class SignUp extends Component {
 		dispatch(getDates());
 	}
 
-	handleChange(e) {
-        console.log('handle change fired')
-        const { dispatch } = this.props;
-        dispatch(onChange(e.target.name, e.target.value))
-    }
+	handleSpeakerName(e) {
+		const { dispatch } = this.props;
+		dispatch(updateSpeakerName(e.target.value));
+	}
+ 	handleEmail(e) {
+		const { dispatch } = this.props;
+		dispatch(updateEmail(e.target.value));
+	}
+ 	handlePhone(e) {
+		const { dispatch } = this.props;
+		dispatch(updatePhone(e.target.value));
+	}
+ 	handleDate(e) {
+		const { dispatch } = this.props;
+		dispatch(updateDate(e.target.value));
+	}
+ 	handleTopic(e) {
+		const { dispatch } = this.props;
+		dispatch(updateTopic(e.target.value));
+	}
+ 	handleDescription(e) {
+		const { dispatch } = this.props;
+		dispatch(updateDescription(e.target.value));
+	}
 
 	submitSpeaker(e) {
 		e.preventDefault();
@@ -52,23 +80,23 @@ class SignUp extends Component {
 								<h3>Speaker Registration</h3>
 								<Field model='user.name'>
 									<label htmlFor='speaker-name'>Name: </label>
-									<input name='speakerName' placeholder='John Smith' id='speaker-firstname' value={speakerName} type='text'  onChange={this.handleChange} required />
+									<input name='speakerName' placeholder='John Smith' id='speaker-firstname' value={speakerName} type='text'  onChange={this.handleSpeakerName} required />
 								</Field >
 								<Field model='user.speaker-email'>
 									<label htmlFor='speaker-email'>Email: </label>
-									<input type="email" placeholder='iamJohnSmith@email.com' name="speakerEmail" value={speakerEmail} required onChange={this.handleChange} />
+									<input type="email" placeholder='iamJohnSmith@email.com' name="speakerEmail" value={speakerEmail} required onChange={this.handleEmail} />
 								</Field>
 								<Field model='user.phone'>
 									<label htmlFor='speaker-phone'>Phone Number: </label>
 									{console.log('this is phone', phone)}
-									<input type="tel" name="phone" placeholder='123-456-7890' required pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" value={phone.replace(/(\d{3})\-?(\d{3})\-?(\d{4})/,'$1-$2-$3')}format="### ### ####" onChange={this.handleChange} />
+									<input type="tel" name="phone" placeholder='123-456-7890' required pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" value={phone.replace(/(\d{3})\-?(\d{3})\-?(\d{4})/,'$1-$2-$3')}format="### ### ####" onChange={this.handlePhone} />
 								</Field>
 								<div>
 									<label htmlFor='event-date'>Date Requested: </label>
-									<select name='date' id='event-date' type='select' required onChange={this.handleChange}>
+									<select name='date' id='event-date' type='select' required onChange={this.handleDate}>
 										<option>Select a Date</option>
 										{events && events.map(event => (
-											<option key={event.date} name='date' value={event.date}>{event.date}</option>
+											<option key={event.date} >{event.date}</option>
 										))}
 									</select>
 								</div>
--- src/components/SignUp/SignUpReducer.js
+++ src/components/SignUp/SignUpReducer.js
@@ -11,10 +11,40 @@ export default function SignUpReducer(state = initialstate, action) {
 	const { payload, type } = action;
 
 	switch (type) {
-		case 'TALK_SUBMIT_FULFILLED': {
+		case 'UPDATE_NAME': {
 			return {
 				...state,
-				submitted: true
+				speakerName: payload
+			}
+		}
+		case 'UPDATE_EMAIL': {
+			return {
+				...state,
+				speakerEmail: payload
+			}
+		}
+		case 'UPDATE_DATE': {
+			return {
+				...state,
+				date: payload
+			}
+		}
+		case 'UPDATE_PHONE': {
+			return {
+				...state,
+				phone: payload
+			}
+		}
+		case 'UPDATE_TOPIC': {
+			return {
+				...state,
+				talkTopic: payload
+			}
+		}
+		case 'UPDATE_DESCRIPTION': {
+			return {
+				...state,
+				description: payload
 			}
 		}
 		case 'GET_DATES_FULFILLED': {
