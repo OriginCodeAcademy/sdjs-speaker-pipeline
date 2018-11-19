13baec89c31b0e9ba66f523b36c1350f874cd9d7
Andrew Prolman <aprolman21@gmail.com>
2018-11-18 16:10:28 -0800

    adds date filters to get talk details and refractors past talks

--- server/utils/getTalkDetails.js
+++ server/utils/getTalkDetails.js
@@ -1,4 +1,5 @@
 const app = require('../server')
+const moment = require('moment');
 
 function getTalkDetails() {
   return new Promise((resolve, reject) => {
@@ -8,33 +9,38 @@ function getTalkDetails() {
         const talkInformation = talks.map((talk) => {
           return Speaker.findById(talk.speakerId)
             .then(speaker => {
+              //console.log('speaker: ', speaker)
               return Event.findById(talk.eventId)
                 .then(selectedEvent => {
                   //if date after today....
-                  return {
-                    speaker: speaker.speakerName,
-                    speakerEmail: speaker.speakerEmail,
-                    speakerPhone: speaker.phone,
-                    topic: talk.topic,
-                    description: talk.description,
-                    talkId: talk.id,
-                    currentStatus: talk.status,
-                    owner: talk.owner,
-                    adminNotes: talk.adminNotes,
-                    eventName: selectedEvent.name,
-                    eventDate: selectedEvent.date,
-                    meetupId: selectedEvent.meetupId,
-                    selectedStatus: undefined,
-                    toggleStatusEdit: false,
-                    toggleOwnerEdit: false,
-                    toggleShowMore: false,
-                    toggleTalkEdit: false,
-                    talkChanges: {
+                  //console.log('events: ', selectedEvent)
+                  
+                  //if(moment(selectedEvent.date).format() > date){
+                    return {
+                      speaker: speaker.speakerName,
+                      speakerEmail: speaker.speakerEmail,
+                      speakerPhone: speaker.phone,
                       topic: talk.topic,
                       description: talk.description,
-                      adminNotes: talk.adminNotes
+                      talkId: talk.id,
+                      currentStatus: talk.status,
+                      owner: talk.owner,
+                      adminNotes: talk.adminNotes,
+                      eventName: selectedEvent.name,
+                      eventDate: selectedEvent.date,
+                      meetupId: selectedEvent.meetupId,
+                      selectedStatus: undefined,
+                      toggleStatusEdit: false,
+                      toggleOwnerEdit: false,
+                      toggleShowMore: false,
+                      toggleTalkEdit: false,
+                      talkChanges: {
+                        topic: talk.topic,
+                        description: talk.description,
+                        adminNotes: talk.adminNotes
+                      }
                     }
-                  }
+                  //}
                 })
                 .catch(err => reject({ error: 'could not find event', err }))
             })
@@ -42,7 +48,18 @@ function getTalkDetails() {
         })
         Promise.all(talkInformation)
         //sort by date try this one first 
-          .then(results => resolve(results))
+          .then(results => {
+            let date = moment().format();
+            //console.log('results: ', results)
+            const filteredResults = results.filter(item => moment(item.eventDate).format() > date)
+            //console.log('filteredResults: ', filteredResults)
+            filteredResults.sort(function(a, b) {
+              a = moment(a.eventDate).format();
+              b = moment(b.eventDate).format();
+              return a<b ? -1 : a>b ? 1 : 0;
+          })
+            resolve(filteredResults)
+          })
           .catch(err => reject({ error: 'could not return results', err }))
       })
       .catch(err => reject({ error: 'could not find pending talks', err }))
