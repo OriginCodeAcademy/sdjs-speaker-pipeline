ba8cb924a49dbb1d800c60637f72889d3e4e9d0e
Tiana Hayden <tiana.hayden@me.com>
2018-11-19 15:44:39 -0800

    fixed issue with choosing none on talks page

--- server/utils/changeTalkOwner.js
+++ server/utils/changeTalkOwner.js
@@ -3,53 +3,51 @@ const app = require('../server');
 function changeTalkOwner(talkId, selectedOwner) {
 	return new Promise((resolve, reject) => {
 		if (talkId == undefined) {
-            reject({ message: 'Bad Talk Id' });
-            return false;
-        }
+			reject({ message: 'Bad Talk Id' });
+			return false;
+		}
 
-        if (selectedOwner == undefined) {
-            reject({ message: 'No Owner Selected' });
-            return false;
-        }
+		if (selectedOwner == undefined) {
+			reject({ message: 'No Owner Selected' });
+			return false;
+		}
 		const { Talk, Organizer } = app.models;
 		Talk.findById(talkId)
 			.then((talk) => {
-				let newTalk;
+				let newTalk = {
+					"status": talk.status,
+					"description": talk.description,
+					"reminderSent": talk.reminderSent,
+					"topic": talk.topic,
+					"id": talk.id,
+					"speakerId": talk.speakerId,
+					"eventId": talk.eventId,
+					"adminNotes": talk.adminNotes
+				}
 				if (selectedOwner == 'None') {
 					newTalk = {
-						"status": talk.status,
-						"description": talk.description,
-						"reminderSent": talk.reminderSent,
-						"topic": talk.topic,
-						"id": talk.id,
-						"speakerId": talk.speakerId,
-						"eventId": talk.eventId,
+						...newTalk,
 						"owner": selectedOwner,
-						"adminNotes": talk.adminNotes
 					}
+					Talk.replaceOrCreate(newTalk)
+						.then(() => resolve(newTalk))
+						.catch(err => reject(err));
 				}
 				else {
-					Organizer.find({ where: {username: selectedOwner}})
-					.then((organizer) => {
-						console.log(organizer)
-						newTalk = {
-							"status": talk.status,
-							"description": talk.description,
-							"reminderSent": talk.reminderSent,
-							"topic": talk.topic,
-							"id": talk.id,
-							"speakerId": talk.speakerId,
-							"eventId": talk.eventId,
-							"owner": selectedOwner,
-							"adminNotes": talk.adminNotes,
-							"organizerId": organizer[0].id
-						}
-					})
-					.catch(err => reject({ error: 'Could not find organizer', err }))
+					Organizer.find({ where: { username: selectedOwner } })
+						.then((organizer) => {
+							console.log(organizer)
+							newTalk = {
+								...newTalk,
+								"owner": selectedOwner,
+								"organizerId": organizer[0].id
+							}
+							Talk.replaceOrCreate(newTalk)
+								.then(() => resolve(newTalk))
+								.catch(err => reject(err));
+						})
+						.catch(err => reject({ error: 'Could not find organizer', err }))
 				}
-				Talk.replaceOrCreate(newTalk)
-					.then(() => resolve(newTalk))
-					.catch(err => reject(err));
 			})
 			.catch(err => reject({ error: 'Could not find talk', err }))
 	})
