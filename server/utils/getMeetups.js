1c723cada82992483a498a9c8059c9b0ec9f656b
Andrew Prolman <aprolman21@gmail.com>
2018-11-19 09:23:10 -0800

    adds parameter to get meetups method

--- server/utils/getMeetups.js
+++ server/utils/getMeetups.js
@@ -5,7 +5,7 @@ function getMeetups() {
 	return new Promise((resolve, reject) => {
 		let currentDate = new Date();
 		currentDate.setMonth(currentDate.getMonth() + 3);
-		axios.get(`https://api.meetup.com/sandiegojs/events?=${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDay()}`)
+		axios.get(`https://api.meetup.com/sandiegojs/events?no_later_than=${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDay()}`)
 			.then(response => {
 				resolve(response.data.map(event => ({
 					meetupId: event.id,
