4852e75238d2c14e2dcd02611cbc5fb6303f79ef
Sam Umirdinov <sam.umirdinov@gmail.com>
2019-08-07 10:21:46 -0700

    clears console logs

--- common/models/organizer.js
+++ common/models/organizer.js
@@ -6,8 +6,6 @@ module.exports = function(Organizer) {
   Organizer.afterRemote('create', function (ctx, modelInstance, next) {
     const username = ctx.result.username;
     const email = ctx.result.email;
-    console.log('this is username: ' + username);
-    console.log('this is email: ' + email);
     sendEmailToNewAdmin(username, email)
         .then(() => next())
         .catch(err => {
