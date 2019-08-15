d93b7d0ce4c60ca29b4c3248051eb36c7eee62af
Sam Umirdinov <sam.umirdinov@gmail.com>
2019-08-15 10:40:50 -0700

    updated edit save and delete features

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,8 +1,7 @@
 'use strict';
 module.exports = {
     "sdjs": {
-      "url": MONGODB_ATLAS_URI,
-      // "url": process.env.MONGODB_URI,
+      "url": process.env.MONGODB_URI,
       "connector": "mongodb"
     }
   }
